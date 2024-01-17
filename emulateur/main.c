bd55841d6481b7a643d5f857de8c1ab61c6c4493
SylvainBRUNET37 <93929648+SylvainBRUNET37@users.noreply.github.com>
2024-01-17 16:39:19 +0100

    debug makefile

--- emulateur/main.c
+++ emulateur/main.c
@@ -3,18 +3,7 @@
 
 int main()
 {
-    uint16_t IRegister = 0;
-    uint8_t x = 144; 
-    
-    IRegister += x / 100;
-    x = x % 100;
-    IRegister >> 4;
-
-    IRegister += x / 10;
-    x = x % 10;
-    IRegister >> 4;
-
-    IRegister += x;
+    uint16_t IRegister = 44;
 
     printf("%d\n", IRegister);
     return 0;
